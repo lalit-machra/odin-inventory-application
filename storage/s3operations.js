d4aed64b589706a2799a573800b43492e7aab96e
lalit-machra <lalitmachra0403@gmail.com>
2025-10-15 11:56:12 +0530

    Manage image storage on AWS S3
    This project uses AWS S3 to store images. Different operations like
    uploading images, deleting images, etc can now be performed. No need to
    store images locally now.

---
+++ storage/s3operations.js
@@ -0,0 +1,95 @@
+import fs from "fs";
+import { 
+  S3Client, PutObjectCommand,
+  DeleteObjectCommand, paginateListObjectsV2, GetBucketLocationCommand,
+  GetObjectCommand, CreateBucketCommand, DeleteBucketCommand
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+async function createBucket(bucketName) {
+  try {
+    const s3client = new S3Client({});
+
+    await s3client.send(
+      new CreateBucketCommand({
+        Bucket: bucketName
+      })
+    );
+    console.log("bucket created successfully");
+  } catch(err) {
+    console.log(err);
+    console.log("some error occured in creating the bucket");
+  }
+};
+
+async function deleteBucket(bucketName) {
+  try {
+    const s3client = new S3Client({});
+
+    const response = await s3client.send(
+      new DeleteBucketCommand({ Bucket: bucketName })
+    );
+    console.log(response);
+    console.log("bucket deletion successful");
+  } catch(err) {
+    console.error(err);
+    console.log("could not delete the bucket");
+  }
+}
+
+async function uploadImg(bucketName, fileName, filePath) {
+  try {
+    const s3client = new S3Client({});
+
+    const fileStream = fs.createReadStream(filePath);
+    const response = await s3client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: fileName,
+        Body: fileStream,
+        ContentType: 'image/png',
+        StorageClass: 'INTELLIGENT_TIERING',
+      })
+    );
+    console.log(response);
+    console.log("image upload successful");
+  } catch(err) {
+    console.log(err);
+    console.log("some error occured in uploading the image");
+  }
+}
+
+async function deleteImg(bucketName, fileName) {
+  try {
+    const s3client = new S3Client({});
+
+    const response = await s3client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: fileName,
+      })
+    );
+    console.log(response);
+    console.log("image deletion successful");
+  } catch(err) {
+    console.log(err);
+    console.log("some error occured in deleting the image");
+  }
+}
+
+async function getImageUrl(bucket, key) {
+  try {
+    const s3client = new S3Client({});
+    let command = new GetObjectCommand({
+      Bucket: bucket,
+      Key: key
+    });
+    let imgUrl = await getSignedUrl(s3client, command, { expiresIn: 3600 });
+    return imgUrl;
+  } catch(err) {
+    console.error(err);
+    console.log("error occured in getting presigned url");
+  }
+}
+
+export { uploadImg, getImageUrl };
