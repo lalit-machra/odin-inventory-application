5da2dd201bf101549b2134df1aeea4a4e82c9e48
lalit-machra <lalitmachra0403@gmail.com>
2025-10-15 12:14:08 +0530

    Display categories on home page

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,5 +1,51 @@
-function getCategories(req, res) {
+import { addCategory, getAllCategories } from "../db/query.js";
+import { uploadImg, getImageUrl } from "../storage/s3operations.js";
 
+async function populateCategories(bucketName, imgFilename, imgPath, imgTitle) {
+  try {
+    await uploadImg(bucketName, imgFilename, imgPath);
+    await addCategory(imgTitle, bucketName, imgFilename);
+    console.log("entry added to categories");
+  } catch(error) {
+    console.error(error);
+    console.log("couldn't add the entry to categories");
+  }
+}
+
+async function getCategories(req, res) {
+  try {
+    const categories = [];
+    let addNew = {};
+    const allCategories = await getAllCategories();
+    // Wait for allCategories.forEach to finish before moving ahead
+    let asyncIterator = new Promise((resolve, reject) => {
+      allCategories.forEach(async (category, index, arr) => {
+        let categ = {};
+        categ.name = category.category;
+        categ.tag = categ.name.toLowerCase().replaceAll(" ", "-");
+        let imgBucket = category.imgbucket;
+        let imgKey = category.imgkey;
+        categ.image = await getImageUrl(imgBucket, imgKey);
+        // 'Add New' should be at last
+        if (categ.name != 'Add New') {
+          categories.push(categ);
+        } else {
+          Object.assign(addNew, categ);
+        }
+        if (index === arr.length - 1) {
+          categories.push(addNew);
+          resolve();
+        }
+      });
+    });
+    asyncIterator.then(() => {
+      res.render("index", { categories: categories });
+    });
+  } catch(err) {
+    console.error(err);
+    console.log("some error occured in displaying categories");
+    res.end();
+  }
 }
 
 function postCategories(req, res) {
